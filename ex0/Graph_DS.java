51bcd73f524cc008414a049e036292991de4aebe
Abed El Kareem Massarweh <abodi.msarwi@gmail.com>
2020-11-07 18:35:05 +0200

    Graph_DS is fixed to utilize the node_data api and implemented by HashSet instead of HashMap, also implemented getV and removeNode methods

--- ex0/Graph_DS.java
+++ ex0/Graph_DS.java
@@ -7,7 +7,7 @@ import java.util.HashSet;
 public class Graph_DS implements graph {
 	
 	
-	private HashMap<node_data, HashSet<node_data>> m_edges = new HashMap<node_data, HashSet<node_data>>();
+	private HashSet<node_data> m_edges = new HashSet<node_data>();
 
 	@Override
 	public node_data getNode(int key) {
@@ -17,38 +17,47 @@ public class Graph_DS implements graph {
 
 	@Override
 	public boolean hasEdge(int node1, int node2) {
-		return m_edges.get(NodeData.getNodeByKey(node1)).contains(NodeData.getNodeByKey(node2));
+		return NodeData.getNodeByKey(node1).hasNi(node2);
 	}
 
 	@Override
 	public void addNode(node_data n) {
-		m_edges.put(n, new HashSet<node_data>());
+		m_edges.add(n);
 	}
 
 	@Override
 	public void connect(int node1, int node2) {
-		// A guard in case the user didn't add the node (Another option is to throw an error)
-		m_edges.putIfAbsent(NodeData.getNodeByKey(node1), new HashSet<node_data>());
+		node_data node_data1 = NodeData.getNodeByKey(node1);
+		node_data node_data2 = NodeData.getNodeByKey(node2);
 		
-		m_edges.get(NodeData.getNodeByKey(node1)).add(NodeData.getNodeByKey(node2));
+		if(node_data1 == null || node_data2 == null || node_data1 == node_data2)
+			return;
+		
+		node_data1.addNi(node_data2);
+		node_data2.addNi(node_data1);
 	}
 
 	@Override
 	public Collection<node_data> getV() {
-		// TODO Auto-generated method stub
-		return null;
+		return m_edges;
 	}
 
 	@Override
 	public Collection<node_data> getV(int node_id) {
-		// TODO Auto-generated method stub
-		return null;
+		node_data node = NodeData.getNodeByKey(node_id);
+		return m_edges.contains(node)? node.getNi(): null;
 	}
 
 	@Override
 	public node_data removeNode(int key) {
-		// TODO Auto-generated method stub
-		return null;
+		node_data node = NodeData.getNodeByKey(key);
+		m_edges.remove(node);
+		
+		for(node_data vertices: m_edges) {
+			vertices.removeNode(node);
+		}
+		
+		return node;
 	}
 
 	@Override

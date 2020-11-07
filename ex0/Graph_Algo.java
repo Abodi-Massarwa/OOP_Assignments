ae6fd710f609714b88c4cf159fae8d7191cb3859
Abed El Kareem Massarweh <abodi.msarwi@gmail.com>
2020-11-07 19:20:43 +0200

    Implement init & copy methods, isConnected is halfway through

--- ex0/Graph_Algo.java
+++ ex0/Graph_Algo.java
@@ -1,25 +1,54 @@
 package ex0;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class Graph_Algo implements graph_algorithms {
+	
+	graph m_graph = new Graph_DS();
 
 	@Override
 	public void init(graph g) {
-		// TODO Auto-generated method stub
-
+		for(node_data n: g.getV())
+			m_graph.addNode(n);
 	}
 
 	@Override
 	public graph copy() {
-		// TODO Auto-generated method stub
-		return null;
+		graph deepCopy = new Graph_DS();
+		
+		HashMap<node_data, node_data> nodeToNodeCopyMapper = new HashMap<node_data, node_data>();
+		for(node_data n: m_graph.getV())
+			nodeToNodeCopyMapper.put(n, new NodeData());
+		
+		for(node_data n: m_graph.getV()) {
+			node_data nCopy = nodeToNodeCopyMapper.get(n);
+			deepCopy.addNode(nCopy);
+			
+			for(node_data m: n.getNi())
+				nCopy.addNi(nodeToNodeCopyMapper.get(m));
+		}
+		
+		return deepCopy;
 	}
 
 	@Override
 	public boolean isConnected() {
-		// TODO Auto-generated method stub
-		return false;
+		int count = 0;
+		
+		for(node_data n: m_graph.getV()) {
+			n.setInfo(NodeData.white);
+		}
+		
+		for(node_data n: m_graph.getV()) {
+			if(n.getInfo() == NodeData.white) {
+				++count;
+				
+				
+			}
+		}
+		
+		return m_graph.getV().size() == count;
 	}
 
 	@Override

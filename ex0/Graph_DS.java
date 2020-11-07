a4a0d92f6803679f350ac85bf720316bab6c69b8
Abed El Kareem Massarweh <abodi.msarwi@gmail.com>
2020-11-07 18:02:33 +0200

    Basic implementation of methods hasEdge, addNode, connect for class Graph_DS

--- ex0/Graph_DS.java
+++ ex0/Graph_DS.java
@@ -1,31 +1,38 @@
 package ex0;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class Graph_DS implements graph {
+	
+	
+	private HashMap<node_data, HashSet<node_data>> m_edges = new HashMap<node_data, HashSet<node_data>>();
 
 	@Override
 	public node_data getNode(int key) {
 		// TODO Auto-generated method stub
-		return null;
+		return NodeData.getNodeByKey(key);
 	}
 
 	@Override
 	public boolean hasEdge(int node1, int node2) {
-		// TODO Auto-generated method stub
-		return false;
+		return m_edges.get(NodeData.getNodeByKey(node1)).contains(NodeData.getNodeByKey(node2));
 	}
 
 	@Override
 	public void addNode(node_data n) {
-		// TODO Auto-generated method stub
-		
+		m_edges.put(n, new HashSet<node_data>());
 	}
 
 	@Override
 	public void connect(int node1, int node2) {
-		// TODO Auto-generated method stub
+		// A guard in case the user didn't add the node (Another option is to throw an error)
+		m_edges.putIfAbsent(NodeData.getNodeByKey(node1), new HashSet<node_data>());
 		
+		m_edges.computeIfPresent(NodeData.getNodeByKey(node1), 
+				(k,v) -> v.add(NodeData.getNodeByKey(node2))?v:v //Returned v because add returns a boolean value
+			);
 	}
 
 	@Override

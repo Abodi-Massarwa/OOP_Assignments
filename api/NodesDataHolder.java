d2e7d1608158df1686838b2633f8701727b3e467
unknown <abodi.msarwi@gmail.com>
2020-12-19 00:20:15 +0200

    Basic components of graph (Nodes & Edges)

---
+++ api/NodesDataHolder.java
@@ -0,0 +1,18 @@
+package api;
+
+import java.util.ArrayList;
+
+public class NodesDataHolder {
+	private static ArrayList<node_data> s_allNdoes = new ArrayList<node_data>();
+	private static int s_keyGenerator = 0;
+	
+	//  Package modifier (default)
+	static int addNode_GetKey(node_data nodeData) {
+		s_allNdoes.add(nodeData);
+		return s_keyGenerator++;
+	}
+	
+	static node_data getNodeByKey(int key) {
+		return s_allNdoes.get(key);
+	}
+}
